970bb10f241837212b187c931afa3a5ce9d29af4
AlexJPrill <alex.j.prill@gmail.com>
2025-03-21 15:12:41 -0400

    added ability to generator astroids

--- tempAlexPrill/AstroidGen.lua
+++ tempAlexPrill/AstroidGen.lua
@@ -6,7 +6,7 @@ end
 
 function AstroidGen:new(speed)
     local width, height = love.graphics.getDimensions()
-    self.x = (width / 2) -1
+    self.x = 0
     self.y = -2
     self.width = 3
     self.height = 3
@@ -21,6 +21,16 @@ end
 
 function AstroidGen:update(dt)
     if not self.spawned then
-        
+       coordinates = {} 
+       if self.pattern == "LINE" then
+
+        coordinates.inserrt({0,0})
+        local x, y = 0, 0
+        while x <= love.graphics.getWidth() do
+            x = x + self.width + 1
+            --coordinates.insert{x, y}
+            table.insert(listOfAstroids, Astroid(x, y, self.speed))
+        end
+    end
     end
 end
