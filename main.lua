8e6658f01044a72bd6bde34749c216dd6027056e
Nicholas Dewberry <coolguynickyd@gmail.com>
2025-03-21 01:01:38 -0400

    fixed so it doesnt restart when we swap scenes.

--- main.lua
+++ main.lua
@@ -17,27 +17,41 @@ local mainmenuref = require("Mainmenuscreen")
 
 function love.load()
 love.window.setMode(609, 812, {resizable=true, vsync=0, minwidth=480, minheight=640})
+love.switchscenes()
+end
+
+function love.switchscenes()
 if gamestate == 0 
 then
 mainmenuref.initializeVals()
   
+-- mainscene is entered: 
+
 elseif gamestate == 1 then 
 playeractions.load()
 
+--end mainscene is entered
+
 elseif gamestate == 2 then
 --blank for now (no death screen) 
+--@TODO: add win screen 
 end
 
 end
-
+  
 function love.update(dt)
 if gamestate == 0 then 
+
+--mainloop controls/physics/interactions
 
 elseif gamestate == 1  then  
 playeractions.update(dt)
 
+--end mainloop controls/physics/interactions
+
 elseif gamestate ==2  then
     --blank for now (no death screen) 
+    --@TODO: add win screen 
   end
 end 
 
@@ -48,13 +62,17 @@ then
 mainmenuref.ShowMainMenu()
 mainmenuref.MoveArrow()
 
+--mainloop draw
 elseif gamestate ==1 
 then
 playeractions.draw()
 
+--end mainloopdraw
+
 elseif gamestate ==2 
 then
   --blank for now (no death screen) 
+  --@TODO: add win screen 
 end
 end
 
@@ -64,13 +82,14 @@ We can't do the simplle thing (iskeydown) because the key goes all the way down.
 So we have to use this love.keypressed thingy. 
 ]]
 function love.keypressed(key, scancode, isrepeat)
+  --we use localscope as a 1-time use variable to switch scenes. It's ugly, but it works.
   localscope =0
   if gamestate == 0  then
   localscope = mainmenuref.keypressed(key)
   end
   if localscope == 1 then
   gamestate = 1 
-  love.load()
+  love.switchscenes()
   end
 
 end
