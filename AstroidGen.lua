906246abc65bc735bed6333d2e2ef0312997ec78
AlexJPrill <alex.j.prill@gmail.com>
2025-03-23 23:22:49 -0400

    Fixed bug finally

--- AstroidGen.lua
+++ AstroidGen.lua
@@ -11,10 +11,10 @@ function AstroidGen:new(speed)
     self.width = 35
     self.height = 3
     self.speed = 90
-    self.patterns = {"LINE", "L", "MBLOCKER"}
-    self.pattern = "LINE"
+    self.patterns = {"L", "MBLOCKER", "SINGLE"}
+    self.pattern = "MBLOCKER"
     self.spawned = false
-    self.coolDown = 2
+    self.coolDown = 1
 end
 
 
@@ -22,16 +22,18 @@ end
 
 function AstroidGen:update(dt)
     if self.coolDown >= 2 then
-       if self.pattern == "LINE" then
+        local rnd = math.random(0,2)
+        self.pattern = self.patterns[rnd]
+        if self.pattern == "LINE" then
+            print("LINE")
+            local x, y = 0, 0
         
-        local x, y = 0, 0
-        
-        while x <= love.graphics.getWidth() do
-            table.insert(listOfAstroids, Astroid(x, y, self.speed))
-            x = x + self.width + 1
-        end
-        self.spawned = true
-        self.coolDown = 0
+            while x <= love.graphics.getWidth() do
+                table.insert(listOfAstroids, Astroid(x, y, self.speed))
+                x = x + self.width + 1
+                end
+            self.spawned = true
+            self.coolDown = 0
         elseif self.pattern == "L" then
             local x, y = 0, 0
             --do stuff to make them spawn in an L shape
@@ -43,15 +45,11 @@ function AstroidGen:update(dt)
                     table.insert(listOfAstroids, Astroid(x, y, self.speed))
                     x = x + self.width + 1
                 end
-
                 local xMod = love.graphics.getWidth()/2
                 for i = 0, 10, 1 do
                     local varyingSpeed = self.speed + (i*10)
                     table.insert(listOfAstroids, Astroid(xMod,y, varyingSpeed))
                 end
-                self.spawned = true
-                self.coolDown = 0
-
             elseif side == 1 then
                 --Wall will be on the left side
                 x = 0
@@ -66,11 +64,13 @@ function AstroidGen:update(dt)
                    local varyingSpeed = self.speed + (i*10)
                    table.insert(listOfAstroids, Astroid(xMod, y, varyingSpeed)) 
                 end
-                self.spawned = true
-                self.coolDown = 0
+            end
+            self.spawned = true
+            self.coolDown = -2
         elseif self.pattern == "MBLOCKER" then
+            math.randomseed(os.time())
             local startX = math.random(0, love.graphics.getWidth()-(4*self.width))
-            startX = 0
+            print(startX)
             local i = 0
             local x, y = 0, 0
             while i < 3 do
@@ -81,12 +81,9 @@ function AstroidGen:update(dt)
             self.spawned = true
             self.coolDown = 0
         end
-            love.graphics.print("Hello world", 100, 100)
-        self.coolDown = self.coolDown + dt
     end
+    self.coolDown = self.coolDown + dt
 end
-self.coolDown = self.coolDown + dt
-end
-
+--self.coolDown = self.coolDown + dt
 
 
